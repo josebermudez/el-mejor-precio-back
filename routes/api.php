f82e8fc98cea0e5c51cd745fbe9d741081e103de
JOSE BERMUDEZ <jbermudez@sproutloud.com>
2018-01-30 13:07:44 -0500

    Primer servicio rest, para obtener la lista de productos

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,6 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('productos', 'ProductosController@show')->middleware('auth');
